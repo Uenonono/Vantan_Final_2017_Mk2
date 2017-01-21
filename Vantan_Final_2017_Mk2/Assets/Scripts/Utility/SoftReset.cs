f65dce164996810ab4533fabc03a0c7cd3781cce
RoDoRiTos <rodoritos_17@i.softbank.jp>
2017-01-21 11:17:23 +0900

    Soft Reset Testing

---
+++ Vantan_Final_2017_Mk2/Assets/Scripts/Utility/SoftReset.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class SoftReset : MonoBehaviour {
+
+	void Awake() {
+    DontDestroyOnLoad(transform.gameObject);
+  }
+
+  void Update() {
+    if(Input.GetAxis("Option") == 1 && Input.GetAxis("Pause") == 1) {
+      var menuObj = GameObject.FindGameObjectWithTag("Menu Selector");
+      if(menuObj != null) {
+        menuObj.GetComponent<ToppingFullCustom.MenuSelector>().Reset();
+      }
+
+      var UDC_GM_Obj = GameObject.FindGameObjectWithTag("UDC Game Manager");
+      if(UDC_GM_Obj != null) {
+        UDC_GM_Obj.GetComponent<UDCommand.GameManager>().Reset();
+      }
+
+      SceneManager.LoadScene("MainTitle");
+    }
+  }
+}
