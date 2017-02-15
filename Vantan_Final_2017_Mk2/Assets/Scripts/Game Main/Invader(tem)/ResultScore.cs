17635c47a3cec0be1a2a128cda9784ef5fd2aade
PS14 <phantasystar2115@yahoo.co.jp>
2017-02-15 12:03:04 +0900

    Result

---
+++ Vantan_Final_2017_Mk2/Assets/Scripts/Game Main/Invader(tem)/ResultScore.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using System.Collections.Generic;
+
+
+
+public class ResultScore : MonoBehaviour
+{
+    public static int SScore;
+    public static int MainScore;
+    int MemoryScore;
+
+
+    void Start()
+    {
+        MainScore = 0;
+        MainScore = Score.score;
+    }
+
+
+    void Update()
+    {
+        if (SScore < MainScore)
+        {
+            SScore += 10;
+        }
+
+        //スコア表示
+        GetComponent<Text>().text = SScore.ToString();
+    }
+}
